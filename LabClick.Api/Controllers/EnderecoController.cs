b56be3c0785a26e34639e6dd302ff7100a5e4477
Ronye do Lago <ronye.rocha@gmail.com>
2018-06-05 12:13:58 -0300

    api endereco

---
+++ LabClick.Api/Controllers/EnderecoController.cs
@@ -0,0 +1,66 @@
+﻿using LabClick.Domain.Entities;
+using LabClick.Infra.Repositories;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LabClick.Api.Controllers
+{
+    [RoutePrefix("endereco")]
+    public class EnderecoController : ApiController
+    {
+        private EnderecoRepository _repository = new EnderecoRepository();
+
+        [Route("enderecos")]
+        public HttpResponseMessage GetEnderecos()
+        {
+            var enderecos = _repository.GetAll();
+
+            return Request.CreateResponse(HttpStatusCode.OK, enderecos);
+        }
+
+        [Route("getById")]
+        public HttpResponseMessage GetById(int id)
+        {
+            var endereco = _repository.GetById(id);
+
+            if (endereco == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, endereco);
+        }
+
+        [HttpPost]
+        [Route("pacientes")]
+        public HttpResponseMessage PostEndereco(Endereco endereco)
+        {
+            if (endereco == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                _repository.Add(endereco);
+
+                return Request.CreateResponse(HttpStatusCode.OK, endereco);
+            }
+            catch (System.Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError,
+                    $"Falha ao incluir o endereço. {ex.Message}");
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repository.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
