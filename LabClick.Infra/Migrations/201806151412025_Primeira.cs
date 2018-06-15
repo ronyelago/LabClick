c0859e0340a2468dad4e47013824758d7b9457ff
Ronye do Lago <ronye.rocha@gmail.com>
2018-06-15 11:17:51 -0300

    Seed complete

---
+++ LabClick.Infra/Migrations/201806151412025_Primeira.cs
@@ -0,0 +1,196 @@
+namespace LabClick.Infra.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Primeira : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clinicas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        LaboratorioId = c.Int(),
+                        EnderecoId = c.Int(),
+                        Nome = c.String(nullable: false, maxLength: 150, unicode: false),
+                        CNPJ = c.String(nullable: false, maxLength: 20, unicode: false),
+                        Email = c.String(maxLength: 50, unicode: false),
+                        Logotipo = c.Binary(),
+                        DataCadastro = c.DateTime(),
+                        DataModificado = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Enderecos", t => t.EnderecoId)
+                .ForeignKey("dbo.Laboratorios", t => t.LaboratorioId)
+                .Index(t => t.LaboratorioId)
+                .Index(t => t.EnderecoId);
+            
+            CreateTable(
+                "dbo.Enderecos",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Cep = c.String(maxLength: 500, unicode: false),
+                        Cidade = c.String(maxLength: 500, unicode: false),
+                        UF = c.String(maxLength: 500, unicode: false),
+                        Numero = c.Int(nullable: false),
+                        Bairro = c.String(maxLength: 500, unicode: false),
+                        Logradouro = c.String(maxLength: 500, unicode: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Laboratorios",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        EnderecoId = c.Int(),
+                        Nome = c.String(nullable: false, maxLength: 100, unicode: false),
+                        Email = c.String(maxLength: 60, unicode: false),
+                        Assinatura = c.Binary(),
+                        DataCadastro = c.DateTime(),
+                        DataModificado = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Enderecos", t => t.EnderecoId)
+                .Index(t => t.EnderecoId);
+            
+            CreateTable(
+                "dbo.Exames",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(nullable: false, maxLength: 150, unicode: false),
+                        ClinicaId = c.Int(nullable: false),
+                        Descricao = c.String(nullable: false, maxLength: 700, unicode: false),
+                        DataCadastro = c.DateTime(),
+                        DataModificado = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Clinicas", t => t.ClinicaId)
+                .Index(t => t.ClinicaId);
+            
+            CreateTable(
+                "dbo.Pacientes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ClinicaId = c.Int(nullable: false),
+                        EnderecoId = c.Int(),
+                        Nome = c.String(nullable: false, maxLength: 150, unicode: false),
+                        Sexo = c.String(nullable: false, maxLength: 10, unicode: false),
+                        Email = c.String(maxLength: 100, unicode: false),
+                        Telefone = c.String(maxLength: 30, unicode: false),
+                        Cpf = c.String(nullable: false, maxLength: 20, unicode: false),
+                        DataNascimento = c.DateTime(precision: 7, storeType: "datetime2"),
+                        DataCadastro = c.DateTime(),
+                        DataModificado = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Clinicas", t => t.ClinicaId)
+                .ForeignKey("dbo.Enderecos", t => t.EnderecoId)
+                .Index(t => t.ClinicaId)
+                .Index(t => t.EnderecoId);
+            
+            CreateTable(
+                "dbo.Resultados",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ExameId = c.Int(nullable: false),
+                        TesteId = c.Int(nullable: false),
+                        Laudo = c.Binary(),
+                        Tabela = c.String(maxLength: 500, unicode: false),
+                        Observacoes = c.String(maxLength: 200, unicode: false),
+                        Documento = c.Binary(),
+                        DataCadastro = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Exames", t => t.ExameId)
+                .ForeignKey("dbo.Testes", t => t.TesteId)
+                .Index(t => t.ExameId)
+                .Index(t => t.TesteId);
+            
+            CreateTable(
+                "dbo.Testes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ExameId = c.Int(nullable: false),
+                        ClinicaId = c.Int(nullable: false),
+                        PacienteId = c.Int(nullable: false),
+                        Imagem = c.Binary(),
+                        Status = c.String(nullable: false, maxLength: 50, unicode: false),
+                        DataCadastro = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Clinicas", t => t.ClinicaId)
+                .ForeignKey("dbo.Exames", t => t.ExameId)
+                .ForeignKey("dbo.Pacientes", t => t.PacienteId)
+                .Index(t => t.ExameId)
+                .Index(t => t.ClinicaId)
+                .Index(t => t.PacienteId);
+            
+            CreateTable(
+                "dbo.Usuarios",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        LaboratorioId = c.Int(nullable: false),
+                        ClinicaId = c.Int(nullable: false),
+                        Nome = c.String(nullable: false, maxLength: 150, unicode: false),
+                        Email = c.String(maxLength: 100, unicode: false),
+                        Senha = c.String(maxLength: 500, unicode: false),
+                        Perfil = c.String(nullable: false, maxLength: 50, unicode: false),
+                        DataCadastro = c.DateTime(),
+                        DataModificado = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Clinicas", t => t.ClinicaId)
+                .ForeignKey("dbo.Laboratorios", t => t.LaboratorioId)
+                .Index(t => t.LaboratorioId)
+                .Index(t => t.ClinicaId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Usuarios", "LaboratorioId", "dbo.Laboratorios");
+            DropForeignKey("dbo.Usuarios", "ClinicaId", "dbo.Clinicas");
+            DropForeignKey("dbo.Resultados", "TesteId", "dbo.Testes");
+            DropForeignKey("dbo.Testes", "PacienteId", "dbo.Pacientes");
+            DropForeignKey("dbo.Testes", "ExameId", "dbo.Exames");
+            DropForeignKey("dbo.Testes", "ClinicaId", "dbo.Clinicas");
+            DropForeignKey("dbo.Resultados", "ExameId", "dbo.Exames");
+            DropForeignKey("dbo.Pacientes", "EnderecoId", "dbo.Enderecos");
+            DropForeignKey("dbo.Pacientes", "ClinicaId", "dbo.Clinicas");
+            DropForeignKey("dbo.Exames", "ClinicaId", "dbo.Clinicas");
+            DropForeignKey("dbo.Clinicas", "LaboratorioId", "dbo.Laboratorios");
+            DropForeignKey("dbo.Laboratorios", "EnderecoId", "dbo.Enderecos");
+            DropForeignKey("dbo.Clinicas", "EnderecoId", "dbo.Enderecos");
+            DropIndex("dbo.Usuarios", new[] { "ClinicaId" });
+            DropIndex("dbo.Usuarios", new[] { "LaboratorioId" });
+            DropIndex("dbo.Testes", new[] { "PacienteId" });
+            DropIndex("dbo.Testes", new[] { "ClinicaId" });
+            DropIndex("dbo.Testes", new[] { "ExameId" });
+            DropIndex("dbo.Resultados", new[] { "TesteId" });
+            DropIndex("dbo.Resultados", new[] { "ExameId" });
+            DropIndex("dbo.Pacientes", new[] { "EnderecoId" });
+            DropIndex("dbo.Pacientes", new[] { "ClinicaId" });
+            DropIndex("dbo.Exames", new[] { "ClinicaId" });
+            DropIndex("dbo.Laboratorios", new[] { "EnderecoId" });
+            DropIndex("dbo.Clinicas", new[] { "EnderecoId" });
+            DropIndex("dbo.Clinicas", new[] { "LaboratorioId" });
+            DropTable("dbo.Usuarios");
+            DropTable("dbo.Testes");
+            DropTable("dbo.Resultados");
+            DropTable("dbo.Pacientes");
+            DropTable("dbo.Exames");
+            DropTable("dbo.Laboratorios");
+            DropTable("dbo.Enderecos");
+            DropTable("dbo.Clinicas");
+        }
+    }
+}
--- LabClick.Infra/Migrations/Configuration.cs
+++ LabClick.Infra/Migrations/Configuration.cs
@@ -14,99 +14,97 @@ namespace LabClick.Infra.Migrations
 
         protected override void Seed(LabClickContext context)
         {
-            //ok
-            Endereco endereco1 = new Endereco
-            {
-                Cep = "05192-289",
-                Cidade = "São Paulo",
-                UF = "SP",
-                Numero = 123,
-                Bairro = "Jardim Marizal",
-                Logradouro = "Rua Carabinani"
-            };
+            //Endereco endereco1 = new Endereco
+            //{
+            //    Cep = "05192-289",
+            //    Cidade = "São Paulo",
+            //    UF = "SP",
+            //    Numero = 123,
+            //    Bairro = "Jardim Marizal",
+            //    Logradouro = "Rua Carabinani"
+            //};
 
-            Endereco endereco2 = new Endereco
-            {
-                Cep = "04192-289",
-                Cidade = "São Paulo",
-                UF = "SP",
-                Numero = 788,
-                Bairro = "Vila Madalena",
-                Logradouro = "Rua Jabaculemos"
-            };
+            //Endereco endereco2 = new Endereco
+            //{
+            //    Cep = "04192-289",
+            //    Cidade = "São Paulo",
+            //    UF = "SP",
+            //    Numero = 788,
+            //    Bairro = "Vila Madalena",
+            //    Logradouro = "Rua Jabaculemos"
+            //};
 
-            Endereco endereco3 = new Endereco
-            {
-                Cep = "07192-289",
-                Cidade = "São Paulo",
-                UF = "SP",
-                Numero = 1544,
-                Bairro = "Butantan",
-                Logradouro = "Rua Três Irmãos"
-            };
+            //Endereco endereco3 = new Endereco
+            //{
+            //    Cep = "07192-289",
+            //    Cidade = "São Paulo",
+            //    UF = "SP",
+            //    Numero = 1544,
+            //    Bairro = "Butantan",
+            //    Logradouro = "Rua Três Irmãos"
+            //};
 
-            Endereco endereco4 = new Endereco
-            {
-                Cep = "09992-289",
-                Cidade = "Taboão da Serra",
-                UF = "SP",
-                Numero = 876,
-                Bairro = "Parque Laguna",
-                Logradouro = "Rua Tom Jobim"
-            };
+            //Endereco endereco4 = new Endereco
+            //{
+            //    Cep = "09992-289",
+            //    Cidade = "Taboão da Serra",
+            //    UF = "SP",
+            //    Numero = 876,
+            //    Bairro = "Parque Laguna",
+            //    Logradouro = "Rua Tom Jobim"
+            //};
 
-            Endereco endereco5 = new Endereco
-            {
-                Cep = "02192-654",
-                Cidade = "São Paulo",
-                UF = "SP",
-                Numero = 543,
-                Bairro = "Caxingui",
-                Logradouro = "Rua Joabe"
-            };
+            //Endereco endereco5 = new Endereco
+            //{
+            //    Cep = "02192-654",
+            //    Cidade = "São Paulo",
+            //    UF = "SP",
+            //    Numero = 543,
+            //    Bairro = "Caxingui",
+            //    Logradouro = "Rua Joabe"
+            //};
 
-            Endereco endereco6 = new Endereco
-            {
-                Cep = "56765-289",
-                Cidade = "São Paulo",
-                UF = "SP",
-                Numero = 423,
-                Bairro = "Jardim Guedala",
-                Logradouro = "Rua Fausto Silva"
-            };
+            //Endereco endereco6 = new Endereco
+            //{
+            //    Cep = "56765-289",
+            //    Cidade = "São Paulo",
+            //    UF = "SP",
+            //    Numero = 423,
+            //    Bairro = "Jardim Guedala",
+            //    Logradouro = "Rua Fausto Silva"
+            //};
 
-            Endereco endereco7 = new Endereco
-            {
-                Cep = "05492-289",
-                Cidade = "São Paulo",
-                UF = "SP",
-                Numero = 988,
-                Bairro = "Parque Bristol",
-                Logradouro = "Rua Silvio Santos"
-            };
+            //Endereco endereco7 = new Endereco
+            //{
+            //    Cep = "05492-289",
+            //    Cidade = "São Paulo",
+            //    UF = "SP",
+            //    Numero = 988,
+            //    Bairro = "Parque Bristol",
+            //    Logradouro = "Rua Silvio Santos"
+            //};
 
-            Endereco endereco8 = new Endereco
-            {
-                Cep = "08976-289",
-                Cidade = "São Paulo",
-                UF = "SP",
-                Numero = 566,
-                Bairro = "Limão",
-                Logradouro = "Rua Nadilson Alves"
-            };
+            //Endereco endereco8 = new Endereco
+            //{
+            //    Cep = "08976-289",
+            //    Cidade = "São Paulo",
+            //    UF = "SP",
+            //    Numero = 566,
+            //    Bairro = "Limão",
+            //    Logradouro = "Rua Nadilson Alves"
+            //};
 
-            context.Endereco.Add(endereco1);
-            context.Endereco.Add(endereco2);
-            context.Endereco.Add(endereco3);
-            context.Endereco.Add(endereco4);
-            context.Endereco.Add(endereco5);
-            context.Endereco.Add(endereco6);
-            context.Endereco.Add(endereco7);
-            context.Endereco.Add(endereco8);
-            context.SaveChanges();
+            //context.Endereco.Add(endereco1);
+            //context.Endereco.Add(endereco2);
+            //context.Endereco.Add(endereco3);
+            //context.Endereco.Add(endereco4);
+            //context.Endereco.Add(endereco5);
+            //context.Endereco.Add(endereco6);
+            //context.Endereco.Add(endereco7);
+            //context.Endereco.Add(endereco8);
+            //context.SaveChanges();
 
-            //ok
-            //Laboratorio lab = new Laboratorio
+            //Laboratorio lab1 = new Laboratorio
             //{
             //    EnderecoId = 1,
             //    Nome = "Laboratório 1",
@@ -115,14 +113,23 @@ namespace LabClick.Infra.Migrations
             //    DataModificado = DateTime.Now
             //};
 
-            //context.Laboratorio.Add(lab);
+            //Laboratorio lab2 = new Laboratorio
+            //{
+            //    EnderecoId = 2,
+            //    Nome = "Laboratório 2",
+            //    Email = "lab2@email.com.br",
+            //    DataCadastro = DateTime.Now,
+            //    DataModificado = DateTime.Now
+            //};
+
+            //context.Laboratorio.Add(lab1);
+            //context.Laboratorio.Add(lab2);
             //context.SaveChanges();
 
-            ////ok
             //Clinica clinica1 = new Clinica
             //{
             //    LaboratorioId = 1,
-            //    EnderecoId = 1,
+            //    EnderecoId = 3,
             //    Nome = "Clínica 1",
             //    CNPJ = "18.080.506/5315-19",
             //    Email = "clinica1@email.com.br",
@@ -130,11 +137,10 @@ namespace LabClick.Infra.Migrations
             //    DataModificado = DateTime.Now
             //};
 
-            ////ok
             //Clinica clinica2 = new Clinica
             //{
-            //    LaboratorioId = 1,
-            //    EnderecoId = 1,
+            //    LaboratorioId = 2,
+            //    EnderecoId = 4,
             //    Nome = "Clínica 2",
             //    CNPJ = "19.619.185/0850-35",
             //    Email = "clinica2@email.com.br",
@@ -146,61 +152,78 @@ namespace LabClick.Infra.Migrations
             //context.Clinica.Add(clinica2);
             //context.SaveChanges();
 
-            ////ok
             //Paciente p1 = new Paciente
             //{
             //    ClinicaId = 1,
-            //    EnderecoId = 1,
+            //    EnderecoId = 5,
             //    Nome = "Jovelino Jovial Jubilane",
             //    Sexo = "Masculino",
             //    Email = "jovelino@email.com.br",
             //    Telefone = "(11) 93456-4523",
-            //    Cpf = "378.987.364-50",
-            //    DataNascimento = DateTime.Now,
+            //    Cpf = "543.987.364-50",
+            //    DataNascimento = new DateTime(1980, 3, 20),
             //    DataCadastro = DateTime.Now,
             //    DataModificado = DateTime.Now
             //};
 
-            ////ok
             //Paciente p2 = new Paciente
             //{
-            //    ClinicaId = 2,
-            //    EnderecoId = 1,
+            //    ClinicaId = 1,
+            //    EnderecoId = 6,
             //    Nome = "Juventina Jovial Jubilane",
             //    Sexo = "Feminino",
             //    Email = "jovelina@email.com.br",
             //    Telefone = "(11) 93456-4523",
-            //    Cpf = "378.987.364-50",
-            //    DataNascimento = DateTime.Now,
+            //    Cpf = "987.987.364-50",
+            //    DataNascimento = new DateTime(1970, 10, 11),
+            //    DataCadastro = DateTime.Now,
+            //    DataModificado = DateTime.Now
+            //};
+
+            //Paciente p3 = new Paciente
+            //{
+            //    ClinicaId = 2,
+            //    EnderecoId = 7,
+            //    Nome = "Marcondes Herculano",
+            //    Sexo = "Masculino",
+            //    Email = "marcondes@email.com.br",
+            //    Telefone = "(11) 93456-4523",
+            //    Cpf = "445.987.364-50",
+            //    DataNascimento = new DateTime(1986, 4, 7),
+            //    DataCadastro = DateTime.Now,
+            //    DataModificado = DateTime.Now
+            //};
+
+            //Paciente p4 = new Paciente
+            //{
+            //    ClinicaId = 2,
+            //    EnderecoId = 8,
+            //    Nome = "Fábio da Silva Júnior",
+            //    Sexo = "Masculino",
+            //    Email = "fabiojr@email.com.br",
+            //    Telefone = "(11) 94567-4523",
+            //    Cpf = "788.987.364-50",
+            //    DataNascimento = new DateTime(1960, 12, 9),
             //    DataCadastro = DateTime.Now,
             //    DataModificado = DateTime.Now
             //};
 
             //context.Paciente.Add(p1);
             //context.Paciente.Add(p2);
+            //context.Paciente.Add(p3);
+            //context.Paciente.Add(p4);
             //context.SaveChanges();
 
-            ////ok
             //Exame exame1 = new Exame
             //{
-            //    Nome = "Glicose",
+            //    Nome = "Zika",
             //    ClinicaId = 1,
-            //    Descricao = "O exame de glicose, também conhecido como teste da glicose, é feito com objetivo de verificar a quantidade de açúcar no sangue, sendo o principal exame realizado para diagnosticar a diabetes.",
+            //    Descricao = "Exame de Zika",
             //    DataCadastro = DateTime.Now,
             //    DataModificado = DateTime.Now
             //};
 
-            ////ok
             //Exame exame2 = new Exame
-            //{
-            //    Nome = "Urina",
-            //    ClinicaId = 1,
-            //    Descricao = "O exame de urina serve para diagnosticar problemas que afetam o sistema renal e urinário, existindo três tipos principais: o exame de urina tipo 1, o exame de urina de 24 horas e o exame de urocultura, que geralmente são realizados num laboratório de análises clínicas e nenhum deles necessita de jejum.",
-            //    DataCadastro = DateTime.Now,
-            //    DataModificado = DateTime.Now
-            //};
-
-            //Exame Exame3 = new Exame
             //{
             //    Nome = "Zika",
             //    ClinicaId = 2,
@@ -219,22 +242,122 @@ namespace LabClick.Infra.Migrations
             //    ClinicaId = 1,
             //    PacienteId = 1,
             //    Imagem = null,
-            //    Status = "Finalizado",
+            //    Status = "Em análise",
             //    DataCadastro = DateTime.Now
             //};
 
             //Teste t2 = new Teste
             //{
-            //    ExameId = 2,
+            //    ExameId = 1,
             //    ClinicaId = 1,
             //    PacienteId = 2,
             //    Imagem = null,
-            //    Status = "Finalizado",
+            //    Status = "Em análise",
+            //    DataCadastro = DateTime.Now
+            //};
+
+            //Teste t3 = new Teste
+            //{
+            //    ExameId = 2,
+            //    ClinicaId = 2,
+            //    PacienteId = 3,
+            //    Imagem = null,
+            //    Status = "Em análise",
+            //    DataCadastro = DateTime.Now
+            //};
+
+            //Teste t4 = new Teste
+            //{
+            //    ExameId = 2,
+            //    ClinicaId = 2,
+            //    PacienteId = 4,
+            //    Imagem = null,
+            //    Status = "Em análise",
             //    DataCadastro = DateTime.Now
             //};
 
             //context.Teste.Add(t1);
             //context.Teste.Add(t2);
+            //context.Teste.Add(t3);
+            //context.Teste.Add(t4);
+            //context.SaveChanges();
+
+            //Resultado r1 = new Resultado
+            //{
+            //    ExameId = 1,
+            //    TesteId = 1,
+            //    Laudo = null,
+            //    Tabela = "",
+            //    Observacoes = "Nenhuma",
+            //    Documento = null,
+            //    DataCadastro = DateTime.Now
+            //};
+
+            //Resultado r2 = new Resultado
+            //{
+            //    ExameId = 1,
+            //    TesteId = 2,
+            //    Laudo = null,
+            //    Tabela = "",
+            //    Observacoes = "Nenhuma",
+            //    Documento = null,
+            //    DataCadastro = DateTime.Now
+            //};
+
+            //Resultado r3 = new Resultado
+            //{
+            //    ExameId = 2,
+            //    TesteId = 3,
+            //    Laudo = null,
+            //    Tabela = "",
+            //    Observacoes = "Nenhuma",
+            //    Documento = null,
+            //    DataCadastro = DateTime.Now
+            //};
+
+            //Resultado r4 = new Resultado
+            //{
+            //    ExameId = 2,
+            //    TesteId = 4,
+            //    Laudo = null,
+            //    Tabela = "",
+            //    Observacoes = "Nenhuma",
+            //    Documento = null,
+            //    DataCadastro = DateTime.Now
+            //};
+
+            //context.Resultado.Add(r1);
+            //context.Resultado.Add(r2);
+            //context.Resultado.Add(r3);
+            //context.Resultado.Add(r4);
+            //context.SaveChanges();
+
+            //Usuario u1 = new Usuario
+            //{
+            //    LaboratorioId = 1,
+            //    ClinicaId = 1,
+            //    Nome = "Ronye do Lago",
+            //    Email = "ronye.rocha@gmail.com",
+            //    Senha= "759310",
+            //    Perfil = "Administrador",
+            //    DataCadastro = DateTime.Now,
+            //    DataModificado = DateTime.Now
+            //};
+
+            //Usuario u2 = new Usuario
+            //{
+            //    LaboratorioId = 2,
+            //    ClinicaId = 2,
+            //    Nome = "Olis do Lago",
+            //    Email = "olis@gmail.com",
+            //    Senha = "759310",
+            //    Perfil = "Administrador",
+            //    DataCadastro = DateTime.Now,
+            //    DataModificado = DateTime.Now
+            //};
+
+            //context.Usuario.Add(u1);
+            //context.Usuario.Add(u2);
             //context.SaveChanges();
         }
     }
