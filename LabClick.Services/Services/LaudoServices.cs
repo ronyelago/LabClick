696960aa8bb378016539b6a9d0577d1fbfe05c58
Ronye do Lago <ronye.rocha@gmail.com>
2018-07-05 16:59:06 -0300

    LaudoServices

---
+++ LabClick.Services/Services/LaudoServices.cs
@@ -0,0 +1,14 @@
+﻿using LabClick.Infra.Repositories;
+
+namespace LabClick.Services.Services
+{
+    class LaudoServices : LaudoRepository
+    {
+        private readonly LaudoRepository repository;
+
+        public LaudoServices()
+        {
+            repository = new LaudoRepository();
+        }
+    }
+}
