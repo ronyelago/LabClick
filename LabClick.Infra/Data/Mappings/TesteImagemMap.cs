2708bbac43228fae9d0548ab1d354fecdf034436
Ronye do Lago <ronye.rocha@gmail.com>
2018-09-17 13:59:34 -0300

    Map

---
+++ LabClick.Infra/Data/Mappings/TesteImagemMap.cs
@@ -0,0 +1,17 @@
+﻿using LabClick.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace LabClick.Infra.Data.Mappings
+{
+    public class TesteImagemMap : EntityTypeConfiguration<TesteImagem>
+    {
+        public TesteImagemMap()
+        {
+            ToTable("TesteImagem");
+
+            HasKey(k => k.Id);
+
+            Property(p => p.Imagem).IsRequired();
+        }
+    }
+}
