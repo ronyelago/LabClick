fd36c497a18a3b238f559715c5d31ce7b9647dad
Ronye do Lago <ronye.rocha@gmail.com>
2018-09-17 14:06:16 -0300

    navigation property Teste

--- LabClick.Domain/Entities/TesteImagem.cs
+++ LabClick.Domain/Entities/TesteImagem.cs
@@ -3,6 +3,8 @@
     public class TesteImagem
     {
         public int Id { get; set; }
+        public int TesteId { get; set; }
         public byte[] Imagem { get; set; }
+        public Teste Teste { get; set; }
     }
 }
