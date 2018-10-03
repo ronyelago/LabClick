f3bf02e6fa8914605060c1d130a4af5337908b15
Ronye do Lago <ronye.rocha@gmail.com>
2018-10-03 12:09:52 -0300

    add lab

--- LabClick.Api/Controllers/LaboratorioController.cs
+++ LabClick.Api/Controllers/LaboratorioController.cs
@@ -34,5 +34,13 @@ namespace LabClick.Api.Controllers
             }
 
         }
+
+        [Route("all")]
+        public HttpResponseMessage GetAll()
+        {
+            var laboratorios = _repository.GetAll();
+
+            return Request.CreateResponse(HttpStatusCode.OK, laboratorios);
+        }
     }
 }
