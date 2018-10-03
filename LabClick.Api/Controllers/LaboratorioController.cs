6ec9219117418f0163a1efdb68d03c70231988e5
Ronye do Lago <ronye.rocha@gmail.com>
2018-10-03 12:02:06 -0300

    add lab

---
+++ LabClick.Api/Controllers/LaboratorioController.cs
@@ -0,0 +1,38 @@
+﻿using LabClick.Domain.Entities;
+using LabClick.Infra.Repositories;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LabClick.Api.Controllers
+{
+    [RoutePrefix("laboratorio")]
+    public class LaboratorioController : ApiController
+    {
+        private LaboratorioRepository _repository = new LaboratorioRepository();
+
+        [HttpPost]
+        [Route("add")]
+        public HttpResponseMessage PostTeste(Laboratorio laboratorio)
+        {
+            if (laboratorio == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                _repository.Add(laboratorio);
+
+                return Request.CreateResponse(HttpStatusCode.OK, laboratorio);
+            }
+            catch (System.Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError,
+                    $"Falha ao incluir o Laboratorio.\n{ex.Message}");
+            }
+
+        }
+    }
+}
