9072ceaf1f52a27323df91a9e1ce16dc20926fec
Ronye do Lago <ronye.rocha@gmail.com>
2018-10-04 11:22:33 -0300

    improve the header

--- LabClick.Services/Services/LaudoServices.cs
+++ LabClick.Services/Services/LaudoServices.cs
@@ -76,23 +76,23 @@ namespace LabClick.Services.Services
 
             //Dados do Paciente
             gfx.DrawString($"Nome: {teste.Paciente.Nome}", font,
-              XBrushes.Black, 35, 150, XStringFormats.Default);
+              XBrushes.Black, 35, 130, XStringFormats.Default);
             gfx.DrawString($"Idade: {DateTimeToAge(teste.Paciente.DataNascimento)} anos", font,
-                XBrushes.Black, 35, 160, XStringFormats.Default);
+                XBrushes.Black, 35, 140, XStringFormats.Default);
             gfx.DrawString($"Cidade: {teste.Paciente.Endereco.Cidade}", font,
-                XBrushes.Black, 35, 170, XStringFormats.Default);
+                XBrushes.Black, 35, 150, XStringFormats.Default);
             gfx.DrawString($"CEP: {teste.Paciente.Endereco.Cep}", font,
-                XBrushes.Black, 35, 180, XStringFormats.Default);
+                XBrushes.Black, 35, 160, XStringFormats.Default);
             gfx.DrawString($"CPF: {teste.Paciente.Cpf}", font,
-                XBrushes.Black, 214, 150, XStringFormats.Default);
+                XBrushes.Black, 244, 130, XStringFormats.Default);
             gfx.DrawString($"Sexo: {teste.Paciente.Sexo}", font,
-                XBrushes.Black, 214, 160, XStringFormats.Default);
+                XBrushes.Black, 244, 140, XStringFormats.Default);
             gfx.DrawString($"UF: {teste.Paciente.Endereco.UF}", font,
-                XBrushes.Black, 214, 170, XStringFormats.Default);
+                XBrushes.Black, 244, 150, XStringFormats.Default);
 
             //Dados do Teste
             gfx.DrawString($"Data do Teste: {teste.DataCadastro}", font,
-                XBrushes.Black, 316, 150, XStringFormats.Default);
+                XBrushes.Black, 346, 130, XStringFormats.Default);
 
             //Resultado e Imagem do Teste
 
