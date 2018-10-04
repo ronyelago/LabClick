c2c8f488204b41f15a457b216cd6ccce95d4012e
Ronye do Lago <ronye.rocha@gmail.com>
2018-10-04 11:31:53 -0300

    improving pdf

--- LabClick.Services/Services/LaudoServices.cs
+++ LabClick.Services/Services/LaudoServices.cs
@@ -63,12 +63,12 @@ namespace LabClick.Services.Services
             gfx.DrawImage(logoImage, 20, 10, 210, 80);
 
             //Body
-            gfx.DrawImage(XImage.FromFile(@"C:\Jobs\labclick\LabClick\Content\styles\images\body.PNG"), 20, 220, 570, 400);
+            gfx.DrawImage(XImage.FromFile(@"C:\Jobs\labclick\LabClick\Content\styles\images\body.PNG"), 20, 220, 570, 380);
 
             //Footer
             Stream footerStream = new MemoryStream(laboratorio.ImagemFooter);
             XImage footerImage = XImage.FromStream(footerStream);
-            gfx.DrawImage(footerImage, 30, 650, 530, 130);
+            gfx.DrawImage(footerImage, 30, 630, 550, 130);
 
             gfx.DrawString("WWW.LABORLABIS.COM.BR", new XFont("Comic Sans", 10), XBrushes.MidnightBlue, 450, 40);
 
@@ -76,23 +76,23 @@ namespace LabClick.Services.Services
 
             //Dados do Paciente
             gfx.DrawString($"Nome: {teste.Paciente.Nome}", font,
-              XBrushes.Black, 35, 130, XStringFormats.Default);
+              XBrushes.Black, 35, 120, XStringFormats.Default);
             gfx.DrawString($"Idade: {DateTimeToAge(teste.Paciente.DataNascimento)} anos", font,
-                XBrushes.Black, 35, 140, XStringFormats.Default);
+                XBrushes.Black, 35, 130, XStringFormats.Default);
             gfx.DrawString($"Cidade: {teste.Paciente.Endereco.Cidade}", font,
-                XBrushes.Black, 35, 150, XStringFormats.Default);
+                XBrushes.Black, 35, 140, XStringFormats.Default);
             gfx.DrawString($"CEP: {teste.Paciente.Endereco.Cep}", font,
-                XBrushes.Black, 35, 160, XStringFormats.Default);
+                XBrushes.Black, 35, 150, XStringFormats.Default);
             gfx.DrawString($"CPF: {teste.Paciente.Cpf}", font,
-                XBrushes.Black, 244, 130, XStringFormats.Default);
+                XBrushes.Black, 244, 120, XStringFormats.Default);
             gfx.DrawString($"Sexo: {teste.Paciente.Sexo}", font,
-                XBrushes.Black, 244, 140, XStringFormats.Default);
+                XBrushes.Black, 244, 130, XStringFormats.Default);
             gfx.DrawString($"UF: {teste.Paciente.Endereco.UF}", font,
-                XBrushes.Black, 244, 150, XStringFormats.Default);
+                XBrushes.Black, 244, 140, XStringFormats.Default);
 
             //Dados do Teste
             gfx.DrawString($"Data do Teste: {teste.DataCadastro}", font,
-                XBrushes.Black, 346, 130, XStringFormats.Default);
+                XBrushes.Black, 410, 120, XStringFormats.Default);
 
             //Resultado e Imagem do Teste
 
