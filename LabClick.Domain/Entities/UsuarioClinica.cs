23425d8f9d62cb9ca5386003967548249fec50c1
Ronye do Lago <ronye.rocha@gmail.com>
2018-10-05 11:26:04 -0300

    UsuarioClinica

---
+++ LabClick.Domain/Entities/UsuarioClinica.cs
@@ -0,0 +1,11 @@
+﻿namespace LabClick.Domain.Entities
+{
+    public class UsuarioClinica
+    {
+        public int UsuarioId { get; set; }
+        public int ClinicaId { get; set; }
+        public virtual Usuario Usuario { get; set; }
+        public virtual  Clinica Clinica { get; set; }
+        public string Perfil { get; set; }
+    }
+}
