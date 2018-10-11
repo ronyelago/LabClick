55626849955649ade8896f69ffa6245229726a76
Ronye do Lago <ronye.rocha@gmail.com>
2018-10-11 14:01:16 -0300

    UsuarioClinicaController

---
+++ LabClick.Api/Controllers/UsuarioClinicaController.cs
@@ -0,0 +1,26 @@
+﻿using LabClick.Infra.Repositories;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LabClick.Api.Controllers
+{
+    [RoutePrefix("usuarioclinica")]
+    public class UsuarioClinicaController : ApiController
+    {
+        private readonly UsuarioClinicaRepository repository = new UsuarioClinicaRepository();
+
+        [Route("getbyuseremail={email}")]
+        public HttpResponseMessage GetByUserEmail(string email)
+        {
+            var user = repository.GetByEmail(email);
+
+            if (user != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, user);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.BadRequest);
+        }
+    }
+}
