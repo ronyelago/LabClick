7ace11e720769c45e7a9b9312d004a95e94a1fe5
Ronye do Lago <ronye.rocha@gmail.com>
2018-11-05 16:14:40 -0200

    Properties

---
+++ LabClick/ViewModel/NovoPacienteViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LabClick.ViewModel
+{
+    public class NovoPacienteViewModel
+    {
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
