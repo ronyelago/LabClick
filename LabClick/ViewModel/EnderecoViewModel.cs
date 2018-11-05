17431e5e2c9b629913a46f52829cdcf81a77721e
Ronye do Lago <ronye.rocha@gmail.com>
2018-11-05 17:14:03 -0200

    Properties

---
+++ LabClick/ViewModel/EnderecoViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LabClick.ViewModel
+{
+    public class EnderecoViewModel
+    {
+        public int Id { get; set; }
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Logradouro { get; set; }
+    }
+}
